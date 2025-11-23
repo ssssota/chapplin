0c084339130c21e13facb839e99b668493567aea
ssssota <tommysouta@gmail.com>
2025-11-23 22:49:42 +0900

    feat: dev

--- packages/chapplin/src/vite/index.ts
+++ packages/chapplin/src/vite/index.ts
@@ -1,7 +1,11 @@
 import type { Plugin } from "vite";
 import { chapplinBuild } from "./build/index.js";
+import { chapplinDev } from "./dev/index.js";
 import type { Options } from "./types.js";
 
 export function chapplin(opts: Options = {}): Plugin[] {
-	return [chapplinBuild(opts)];
+	return [
+		chapplinBuild(opts),
+		// ...chapplinDev(opts)
+	];
 }
