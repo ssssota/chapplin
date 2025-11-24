0fedfd5d827127691e837dc2b30b5525eaf8ad3c
ssssota <tommysouta@gmail.com>
2025-11-25 00:26:57 +0900

    refactor: organize plugins

--- packages/chapplin/src/vite/index.ts
+++ packages/chapplin/src/vite/index.ts
@@ -1,8 +1,9 @@
 import type { Plugin } from "vite";
-import { chapplinBuild } from "./build/index.js";
-import { chapplinDev } from "./dev/index.js";
+import { build } from "./plugins/build.js";
+import { clientToolResolver } from "./plugins/client-tool-resolver.js";
+import { devServer } from "./plugins/dev-server.js";
 import type { Options } from "./types.js";
 
 export function chapplin(opts: Options = {}): Plugin[] {
-	return [chapplinBuild(opts), ...chapplinDev(opts)];
+	return [build(opts), clientToolResolver(opts), devServer()];
 }
