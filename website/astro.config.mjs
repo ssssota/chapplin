ebf64e957fe4acd36c65ce190534cfd63287064e
ssssota <tommysouta@gmail.com>
2025-11-30 22:22:36 +0900

    feat(website): initialize astro/starlight

---
+++ website/astro.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "Chapplin",
+			logo: { src: "./public/logo.svg", replacesTitle: true },
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/ssssota/chapplin",
+				},
+			],
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: "Example Guide", slug: "guides/example" },
+					],
+				},
+				{
+					label: "Reference",
+					autogenerate: { directory: "reference" },
+				},
+			],
+		}),
+	],
+});
