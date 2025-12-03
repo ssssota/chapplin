1eccbe48addf1cfea9c85876f4d0a38b9b3fdedb
ssssota <tommysouta@gmail.com>
2025-12-04 00:28:47 +0900

    chore(website): remove base

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	base: "/chapplin/",
 	integrations: [
 		starlight({
 			title: "Chapplin",
